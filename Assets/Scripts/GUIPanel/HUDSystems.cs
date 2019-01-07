6aa4316e3acf8b55ec1caab9e9a9ccec356da79f
Cezary Janiak <cezaryjaniak01@gmail.com>
2019-01-07 19:44:41 +0100

    Deleted files and reset git history to use lfs
    Preparing to exclude large files by lfs and unnecessary
    by gitignore. Commit Driven Development applying.

--- Assets/Scripts/GUIPanel/HUDSystems.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HUDSystems : MonoBehaviour
-{
-    static public int theJumps = PlayerShip.S.lifes;
-}
